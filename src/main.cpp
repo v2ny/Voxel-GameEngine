1c4e227bba769dbd7f8530c0dc0fdf8dc7ff0090
AcidBunny <99141995+Suffected@users.noreply.github.com>
2022-04-18 03:35:05 +0200

    Added our first triangle, changed CMakeLists.txt and Organized the shaders (First triangle was from Victor Gordan Tutorials)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,35 @@
 #include <GLFW/glfw3.h>
 #include <floatsToRGB.h>
 #include <BoxLogger.h>
+#include <cmath>
+
+#include <ShaderClass.h>
+#include <Organizing/VAO/VAO.h>
+#include <Organizing/VBO/VBO.h>
+#include <Organizing/EBO/EBO.h>
 
 int wWidth = 1024;
 int wHeight = 600;
 const char* wTitle = "Voxel-GameEngine";
+
+// Vertices coordinates
+GLfloat vertices[] =
+{
+	-0.5f, -0.5f * float(std::sqrt(3)) / 3, 0.0f, // Lower left corner
+	0.5f, -0.5f * float(std::sqrt(3)) / 3, 0.0f, // Lower right corner
+	0.0f, 0.5f * float(std::sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+	-0.5f / 2, 0.5f * float(std::sqrt(3)) / 6, 0.0f, // Inner left
+	0.5f / 2, 0.5f * float(std::sqrt(3)) / 6, 0.0f, // Inner right
+	0.0f, -0.5f * float(std::sqrt(3)) / 3, 0.0f // Inner down
+};
+
+// Indices for vertices order
+GLuint indices[] =
+{
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
 
 int main()
 {
@@ -39,6 +64,7 @@ int main()
         std::cout << "Failed to initialize GLAD!\n";
         return -1;
     }  
+    gladLoadGL();
 
     // Let's implement our opengl/window viewport
     glViewport(0, 0, wWidth, wHeight);
@@ -49,16 +75,47 @@ int main()
     // Send a test MessageBox with our new function
     // box_l.Log("A basic message", "message", nullptr, box_l.info);
 
+    Shader shaderProgram("VertexShader.glsl", "FragmentShader.glsl");
+
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+
+	// Links VBO to VAO
+	VAO1.LinkVBO(VBO1, 0);
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
     while(!glfwWindowShouldClose(window))
     {
         // Let's clear our color buffer bit to get our background color appear!
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shaderProgram.Activate();
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         // Important to get our window working properly!
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
+    // Delete window before ending the program
+	glfwDestroyWindow(window);
     // To exit the window when it leaves the while loop (It leaves the while loop if there's an close/crash interupt)
     glfwTerminate();
     return 0;
---
+++ src/pub/Shaders/Source/FragmentShader.glsl
@@ -0,0 +1,8 @@
+#version 330 core
+
+out vec4 FragmentColor;
+
+void main()
+{
+    FragmentColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+}
