c74384a57675a90da543a67a9307b12a23416d29
AcidBunny <99141995+Suffected@users.noreply.github.com>
2022-04-20 22:36:38 +0200

    Made changes on CMakeLists.txt added glm in it, Corrected function in Texture.cpp & Texture.h

--- src/GameEngine/TextureClass/Texture.cpp
+++ src/GameEngine/TextureClass/Texture.cpp
@@ -41,7 +41,7 @@ Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, 
     glBindTexture(texType, 0);
 }
 
-void Texture::texUnit(Shader shader, const char* uniform, GLuint unit)
+void Texture::texUnit(Shader& shader, const char* uniform, GLuint unit)
 {
     GLuint texUni = glGetUniformLocation(shader.ID, uniform);
     shader.Activate();
--- src/GameEngine/TextureClass/Texture.h
+++ src/GameEngine/TextureClass/Texture.h
@@ -13,7 +13,7 @@ class Texture
         GLenum type;
         Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
 
-        void texUnit(Shader shader, const char* uniform, GLuint unit);
+        void texUnit(Shader& shader, const char* uniform, GLuint unit);
         void Bind();
         void Unbind();
         void Delete();
